23a1c1e80ab421e840bb773cc447f8ed6b8db1cd
hae-wong <89lhwon@gmail.com>
2026-01-22 20:07:18 +0900

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
-import styled, { keyframes } from "styled-components";
+import { useState } from "react";
+import styled from "styled-components";
 
-const Title = styled.h2`
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+
+const H1 = styled.h1`
   color:${(props) => props.theme.textColor};
 `;
 
-const Wrapper = styled.div`
-  display:flex;
-  height:100vh;
-  width:100vw;
-  justify-content:center;
-  align-items:center;
-  background-color:${(props) => props.theme.backgroundColor}
-`;
+interface DummyProps{
+  text:string;
+  active?:boolean;
+}
 
+function Dummy({text, active = false}:DummyProps) {
+  return <H1>{text}</H1>
+}
 
 function App() {
+  const onClick = (event:React.FormEvent<HTMLButtonElement>) => {
+    
+  };
   return (
-    <Wrapper>
-      <Title>Hello</Title>
-    </Wrapper>
+    <Container>
+      <Dummy active text="hello" />
+      <form>
+        <button onClick={onClick}>click me</button>
+      </form>
+    </Container>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ThemeProvider } from 'styled-components';
 import App from './App';
+import { ThemeProvider } from 'styled-components';
+import { darkTheme, lightTheme } from './theme';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-
-const darkTheme = {
-  textColor: "whitesmoke",
-  backgroundColor :"#111"
-}
-
-const lightTheme = {
-  textColor: "#111",
-  backgroundColor :"whitesmoke"
-}
 
 root.render(
   <React.StrictMode>
