c2af8502f1a275a381524dba8ea107de080a8206
hae-wong <89lhwon@gmail.com>
2026-02-23 23:14:34 +0900

    chart update

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -22,66 +22,92 @@ function Chart({ coinId }: ChartProps) {
         { queryKey: ["ohlcv", coinId], queryFn: () => fetchCoinHistory(coinId) }
     );
     return <div>{isLoading ? (
-        "Loading char..."
+        "Loading chart..."
     ) : (
+        // "Chart"
         <ApexChart
-            type="line"
+            type="candlestick"
             series={[
                 {
-                    name: "Price",
-                    data: data?.map((price) => Number(price.close)) ?? [],
-                },
+                    name: "StockPrice",
+                    data: data?.map((price) => ({
+                        x: new Date(price.time_close),
+                        y: [price.open, price.high, price.low, price.close],
+                    })) ?? [],
+                }
             ]}
             options={{
                 theme: {
                     mode: "dark"
                 },
                 chart: {
-                    height: 300,
-                    width: 500,
                     toolbar: {
                         show: false,
-                    },
-                    background: "transparent",
-                },
-                grid: {
-                    show: false,
-                },
-                stroke: {
-                    curve: "smooth",
-                    width: 4,
-                },
-                yaxis: {
-                    show: false,
+                    }
                 },
                 xaxis: {
-                    axisBorder: {
-                        show: false
-                    },
-                    axisTicks: {
-                        show: false,
-                    },
-                    labels: {
-                        show: false,
-                    },
-                    categories : data?.map((price) => Number(price.time_close)) ?? [],
-                    type: "datetime",
+                    type: 'datetime'
                 },
-                fill: {
-                    type: "gradient",
-                    gradient: {
-                        gradientToColors: ["#4cd137"],
-                        stops: [0, 100]
-                    },
-                    colors: ["#00a8ff"],
-                },
-                tooltip: {
-                    y : {
-                        formatter: (value) => `$ ${value.toFixed(2)}`,
-                    },
-                }
             }}
         />
+        // <ApexChart
+        //     type="line"
+        //     series={[
+        //         {
+        //             name: "Price",
+        //             data: data?.map((price) => Number(price.close)) ?? [],
+        //         },
+        //     ]}
+        //     options={{
+        //         theme: {
+        //             mode: "dark"
+        //         },
+        //         chart: {
+        //             height: 300,
+        //             width: 500,
+        //             toolbar: {
+        //                 show: false,
+        //             },
+        //             background: "transparent",
+        //         },
+        //         grid: {
+        //             show: false,
+        //         },
+        //         stroke: {
+        //             curve: "smooth",
+        //             width: 4,
+        //         },
+        //         yaxis: {
+        //             show: false,
+        //         },
+        //         xaxis: {
+        //             axisBorder: {
+        //                 show: false
+        //             },
+        //             axisTicks: {
+        //                 show: false,
+        //             },
+        //             labels: {
+        //                 show: false,
+        //             },
+        //             categories : data?.map((price) => Number(price.time_close)) ?? [],
+        //             type: "datetime",
+        //         },
+        //         fill: {
+        //             type: "gradient",
+        //             gradient: {
+        //                 gradientToColors: ["#4cd137"],
+        //                 stops: [0, 100]
+        //             },
+        //             colors: ["#00a8ff"],
+        //         },
+        //         tooltip: {
+        //             y : {
+        //                 formatter: (value) => `$ ${value.toFixed(2)}`,
+        //             },
+        //         }
+        //     }}
+        // />
     )}</div>
 }
 
